68c7c257dc0008f8412eb432fdb04effa1a93db5
giacomo <giacomo.franchini@polito.it>
2024-02-20 17:31:39 +0100

    launch file clean up

--- launch/topic_synchronizer.launch.py
+++ launch/topic_synchronizer.launch.py
@@ -1,8 +1,5 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import ComposableNodeContainer, Node, SetParameter
+from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 
 from ament_index_python.packages import get_package_share_directory
@@ -23,33 +20,9 @@ def generate_launch_description():
                 parameters=[os.path.join(get_package_share_directory("topic_synchronizer"), 'params', 'params.yaml')],
                 extra_arguments=[{'use_intra_process_comms': True}],
             ),
-            ComposableNode(
-                package='rosbag2_transport',
-                plugin='rosbag2_transport::Player',
-                name='player',
-                parameters=[os.path.join(get_package_share_directory("etna_s3li_playback"), 'config', 'rosbag2_player.yaml')],
-                extra_arguments=[{'use_intra_process_comms': True}],
-            ),
         ],
         output='both',
     )
 
-    rviz2 = Node(
-        package='rviz2',
-        executable='rviz2',
-        output={'both': 'log'},
-        arguments=[
-            '-d', PathJoinSubstitution([
-                os.path.join(get_package_share_directory("etna_s3li_playback"), 'rviz', 'roxy_rviz_config.rviz')
-            ]),
-        ],
-        condition=IfCondition(LaunchConfiguration('visualize')),
-    )
-
-    return LaunchDescription([
-        SetParameter(name='use_sim_time', value=True),
-        DeclareLaunchArgument('visualize', default_value='false'),
-        rviz2,
-        container,
-    ])
+    return LaunchDescription([container])
 
