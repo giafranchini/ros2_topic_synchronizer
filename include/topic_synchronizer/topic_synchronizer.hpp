464cb7030c2693e49ab086e5673006dc9f7f98b1
giacomo <giacomo.franchini@polito.it>
2024-11-11 16:12:54 +0100

    configuration for pinn synchronization

--- include/topic_synchronizer/topic_synchronizer.hpp
+++ include/topic_synchronizer/topic_synchronizer.hpp
@@ -6,8 +6,11 @@ Copyright 2024 Giacomo Franchini
 #define TOPIC_SYNCHRONIZER__TOPIC_SYNCHRONIZER_HPP_
 
 #include <geometry_msgs/msg/transform_stamped.h>
+#include <geometry_msgs/msg/twist_stamped.h>
 #include <sensor_msgs/msg/imu.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
 #include <nav_msgs/msg/odometry.hpp>
+#include <jackal_msgs/msg/feedback.hpp>
 
 #include <array>
 #include <memory>
@@ -30,32 +33,47 @@ public:
   ~TopicSynchronizer() {}
 
 private:
-  std::string odom_source_topic_, odom_dest_topic_;
-  std::string odom_gt_source_topic_, odom_gt_dest_topic_;
-  std::string imu_source_topic_, imu_dest_topic_;
+  std::string cmd_vel_source, feedback_source, imu_source, joint_state_source, odom_source, odom_filtered_source, odom_gt_source;
+  std::string cmd_vel_out, feedback_out, imu_out, joint_state_out, odom_out, odom_filtered_out, odom_gt_out;
   
-  float interval_duration_, approx_policy_;
+  float interval_duration, approx_policy;
 
   // Define subscribers to topics that we want to synchronize
-  std::shared_ptr<message_filters::Subscriber<nav_msgs::msg::Odometry>> odom_subs_;
-  std::shared_ptr<message_filters::Subscriber<nav_msgs::msg::Odometry>> odom_gt_subs_;
-  std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::Imu>> imu_subs_;
+  std::shared_ptr<message_filters::Subscriber<geometry_msgs::msg::Twist>> cmd_vel_subs;
+  std::shared_ptr<message_filters::Subscriber<jackal_msgs::msg::Feedback>> feedback_subs;
+  std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::JointState>> joint_state_subs;
+  std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::Imu>> imu_subs;
+  std::shared_ptr<message_filters::Subscriber<nav_msgs::msg::Odometry>> odom_subs;
+  std::shared_ptr<message_filters::Subscriber<nav_msgs::msg::Odometry>> odom_filtered_subs;
+  std::shared_ptr<message_filters::Subscriber<nav_msgs::msg::Odometry>> odom_gt_subs;
 
   using approximate_policy = message_filters::sync_policies::ApproximateTime<
+    geometry_msgs::msg::Twist,
+    jackal_msgs::msg::Feedback,
+    sensor_msgs::msg::JointState,
+    sensor_msgs::msg::Imu,
     nav_msgs::msg::Odometry,
     nav_msgs::msg::Odometry,
-    sensor_msgs::msg::Imu>;
+    nav_msgs::msg::Odometry>;
   
-  std::shared_ptr<message_filters::Synchronizer<approximate_policy>> time_sync_;
+  std::shared_ptr<message_filters::Synchronizer<approximate_policy>> time_sync;
 
-  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
-  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_gt_pub_;
-  rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub;
+  rclcpp::Publisher<jackal_msgs::msg::Feedback>::SharedPtr feedback_pub;
+  rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_state_pub;
+  rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub;
+  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub;
+  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_filtered_pub;
+  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_gt_pub;
 
   void ts_callback(
+    const geometry_msgs::msg::Twist::ConstSharedPtr & cmd_vel_msg,
+    const jackal_msgs::msg::Feedback::ConstSharedPtr & feedback_msg,
+    const sensor_msgs::msg::JointState::ConstSharedPtr & joint_state_msg,
+    const sensor_msgs::msg::Imu::ConstSharedPtr & imu_msg,
     const nav_msgs::msg::Odometry::ConstSharedPtr & odom_msg,
-    const nav_msgs::msg::Odometry::ConstSharedPtr & odom_gt_msg,
-    const sensor_msgs::msg::Imu::ConstSharedPtr & imu_msg);
+    const nav_msgs::msg::Odometry::ConstSharedPtr & odom_filtered_msg,
+    const nav_msgs::msg::Odometry::ConstSharedPtr & odom_gt_msg);
 };
 }  // namespace synchronizer
 
--- src/topic_synchronizer.cpp
+++ src/topic_synchronizer.cpp
@@ -19,53 +19,99 @@ namespace synchronizer
 using std::placeholders::_1;
 using std::placeholders::_2;
 using std::placeholders::_3;
+using std::placeholders::_4;
+using std::placeholders::_5;
+using std::placeholders::_6;
+using std::placeholders::_7;
 using std::chrono::steady_clock;
 
 TopicSynchronizer::TopicSynchronizer(const rclcpp::NodeOptions & options)
 : Node("topic_synchronizer", options)
 {
-  odom_source_topic_ = declare_parameter<std::string>("topics.odom_source", "/odometry");
-  odom_gt_source_topic_ = declare_parameter<std::string>("topics.odom_gt_source", "/odometry_gt");
-  imu_source_topic_ = declare_parameter<std::string>("topics.imu_source", "/imu");
-  odom_dest_topic_ = declare_parameter<std::string>("topics.odom_output", "/odometry_sync");
-  odom_gt_dest_topic_ = declare_parameter<std::string>("topics.odom_gt_output", "/odometry_gt_sync");
-  imu_dest_topic_ = declare_parameter<std::string>("topics.imu_output", "/imu_sync");
-  interval_duration_ = declare_parameter<float>("interval_duration", 500000000.0);
-  approx_policy_ = declare_parameter<double>("approx_policy", 10.0);
+  cmd_vel_source = declare_parameter<std::string>("topics.source.cmd_vel", "/cmd_vel");
+  feedback_source = declare_parameter<std::string>("topics.source.feedback", "/feedback");
+  joint_state_source = declare_parameter<std::string>("topics.source.joint_state", "/joint_states");
+  imu_source = declare_parameter<std::string>("topics.source.imu", "/imu");
+  odom_source = declare_parameter<std::string>("topics.source.odom", "/odometry");
+  odom_filtered_source = declare_parameter<std::string>("topics.source.odom_filtered", "/odometry_filtered");
+  odom_gt_source = declare_parameter<std::string>("topics.source.odom_gt", "/odometry_gt");
 
-  odom_pub_ = create_publisher<nav_msgs::msg::Odometry>(odom_dest_topic_, 10);
-  odom_gt_pub_ = create_publisher<nav_msgs::msg::Odometry>(odom_gt_dest_topic_, 10);
-  imu_pub_ = create_publisher<sensor_msgs::msg::Imu>(imu_dest_topic_, 10);
-  odom_subs_ = std::make_shared<message_filters::Subscriber<nav_msgs::msg::Odometry>>(this, odom_source_topic_);
-  odom_gt_subs_ = std::make_shared<message_filters::Subscriber<nav_msgs::msg::Odometry>>(this, odom_gt_source_topic_);
-  imu_subs_ = std::make_shared<message_filters::Subscriber<sensor_msgs::msg::Imu>>(this, imu_source_topic_);
-  time_sync_ = std::make_shared<message_filters::Synchronizer<approximate_policy>>(
-    approximate_policy(approx_policy_),
-    *odom_subs_,
-    *odom_gt_subs_,
-    *imu_subs_);
-  time_sync_->setMaxIntervalDuration(rclcpp::Duration(0, interval_duration_));
-  time_sync_->registerCallback(std::bind(&TopicSynchronizer::ts_callback, this, _1, _2, _3));
+  cmd_vel_out = declare_parameter<std::string>("topics.output.cmd_vel", "/cmd_vel_sync");
+  feedback_out = declare_parameter<std::string>("topics.output.feedback", "/feedback_sync");
+  joint_state_out = declare_parameter<std::string>("topics.output.joint_state", "/joint_states_sync");
+  imu_out = declare_parameter<std::string>("topics.output.imu", "/imu_sync");
+  odom_out = declare_parameter<std::string>("topics.output.odom", "/odometry_sync");
+  odom_filtered_out = declare_parameter<std::string>("topics.output.odom_filtered", "/odometry_filtered_sync");
+  odom_gt_out = declare_parameter<std::string>("topics.output.odom_gt", "/odometry_gt_sync");
+
+  interval_duration = declare_parameter<float>("interval_duration", 500000000.0);
+  approx_policy = declare_parameter<double>("approx_policy", 10.0);
+
+  cmd_vel_pub = create_publisher<geometry_msgs::msg::Twist>(cmd_vel_out, 10);
+  feedback_pub = create_publisher<jackal_msgs::msg::Feedback>(feedback_out, 10);
+  joint_state_pub = create_publisher<sensor_msgs::msg::JointState>(joint_state_out, 10);
+  imu_pub = create_publisher<sensor_msgs::msg::Imu>(imu_out, 10);
+  odom_pub = create_publisher<nav_msgs::msg::Odometry>(odom_out, 10);
+  odom_filtered_pub = create_publisher<nav_msgs::msg::Odometry>(odom_filtered_out, 10);
+  odom_gt_pub = create_publisher<nav_msgs::msg::Odometry>(odom_gt_out, 10);
+
+  cmd_vel_subs = std::make_shared<message_filters::Subscriber<geometry_msgs::msg::Twist>>(this, cmd_vel_source);
+  feedback_subs = std::make_shared<message_filters::Subscriber<jackal_msgs::msg::Feedback>>(this, feedback_source);
+  joint_state_subs = std::make_shared<message_filters::Subscriber<sensor_msgs::msg::JointState>>(this, joint_state_source);
+  imu_subs = std::make_shared<message_filters::Subscriber<sensor_msgs::msg::Imu>>(this, imu_source);
+  odom_subs = std::make_shared<message_filters::Subscriber<nav_msgs::msg::Odometry>>(this, odom_source);
+  odom_filtered_subs = std::make_shared<message_filters::Subscriber<nav_msgs::msg::Odometry>>(this, odom_filtered_source);
+  odom_gt_subs = std::make_shared<message_filters::Subscriber<nav_msgs::msg::Odometry>>(this, odom_gt_source);
+
+  time_sync = std::make_shared<message_filters::Synchronizer<approximate_policy>>(
+    approximate_policy(approx_policy),
+    *cmd_vel_subs,
+    *feedback_subs,
+    *joint_state_subs,
+    *imu_subs,
+    *odom_subs,
+    *odom_filtered_subs,
+    *odom_gt_subs);
+
+  time_sync_->setMaxIntervalDuration(rclcpp::Duration(0, interval_duration));
+  time_sync_->registerCallback(
+    std::bind(&TopicSynchronizer::ts_callback, this, _1, _2, _3, _4, _5, _6, _7));
 }
 
 void TopicSynchronizer::ts_callback(
+  const geometry_msgs::msg::Twist::ConstSharedPtr & cmd_vel_msg,
+  const jackal_msgs::msg::Feedback::ConstSharedPtr & feedback_msg,
+  const sensor_msgs::msg::JointState::ConstSharedPtr & joint_state_msg,
+  const sensor_msgs::msg::Imu::ConstSharedPtr & imu_msg,
   const nav_msgs::msg::Odometry::ConstSharedPtr & odom_msg,
-  const nav_msgs::msg::Odometry::ConstSharedPtr & odom_gt_msg,
-  const sensor_msgs::msg::Imu::ConstSharedPtr & imu_msg)
+  const nav_msgs::msg::Odometry::ConstSharedPtr & odom_filtered_msg,
+  const nav_msgs::msg::Odometry::ConstSharedPtr & odom_gt_msg)
 {
-  rclcpp::Time odom_time = odom_msg->header.stamp;
-  rclcpp::Time odom_gt_time = odom_gt_msg->header.stamp;
+  rclcpp::Time cmd_vel_time = cmd_vel_msg->header.stamp;
+  rclcpp::Time feedback_time = feedback_msg->header.stamp;
+  rclcpp::Time joint_state_time = joint_state_msg->header.stamp;
   rclcpp::Time imu_time = imu_msg->header.stamp;
+  rclcpp::Time odom_time = odom_msg->header.stamp;
+  rclcpp::Time odom_filtered_time = odom_filtered_msg->header.stamp;
+  rclcpp::Time odom_gt_time = odom_gt_msg->header.stamp;
   
   RCLCPP_DEBUG(get_logger(), "Received messages");
   RCLCPP_DEBUG(get_logger(), 
     "Publishing messages at time: \n%f \n%f \n%f", 
+    cmd_vel_msg->header.stamp.sec + cmd_vel_msg->header.stamp.nanosec * 1e-9,
+    feedback_msg->header.stamp.sec + feedback_msg->header.stamp.nanosec * 1e-9,
+    joint_state_msg->header.stamp.sec + joint_state_msg->header.stamp.nanosec * 1e-9,
+    imu_msg->header.stamp.sec + imu_msg->header.stamp.nanosec * 1e-9,
     odom_msg->header.stamp.sec + odom_msg->header.stamp.nanosec * 1e-9,
-    odom_gt_msg->header.stamp.sec + odom_gt_msg->header.stamp.nanosec * 1e-9,
-    imu_msg->header.stamp.sec + imu_msg->header.stamp.nanosec * 1e-9);
+    odom_filtered_msg->header.stamp.sec + odom_filtered_msg->header.stamp.nanosec * 1e-9,
+    odom_gt_msg->header.stamp.sec + odom_gt_msg->header.stamp.nanosec * 1e-9);
  
-  odom_pub_->publish(*odom_msg);
-  odom_gt_pub_->publish(*odom_gt_msg);
+  cmd_vel_pub_->publish(*cmd_vel_msg);
+  feedback_pub_->publish(*feedback_msg);
+  joint_state_pub_->publish(*joint_state_msg);
   imu_pub_->publish(*imu_msg);
+  odom_pub_->publish(*odom_msg);
+  odom_filtered_pub_->publish(*odom_filtered_msg);
+  odom_gt_pub_->publish(*odom_gt_msg);
 }
 }  // namespace synchronizer
