afd197ff2e1028d59be9e8ec8bfa3e19a6037f46
giacomo <giacomo.franchini@polito.it>
2024-11-20 14:07:57 +0100

    synchronize 2 odometry topics

--- include/topic_synchronizer/topic_synchronizer.hpp
+++ include/topic_synchronizer/topic_synchronizer.hpp
@@ -28,8 +28,8 @@ public:
   ~TopicSynchronizer() {}
 
 private:
-  std::string odom_source0, odom_source1, odom_source2;
-  std::string odom_out0, odom_out1, odom_out2;
+  std::string odom_source0, odom_source1;
+  std::string odom_out0, odom_out1;
   
   float interval_duration;
   int approx_policy;
@@ -37,10 +37,8 @@ private:
   // Define subscribers to topics that we want to synchronize
   std::shared_ptr<message_filters::Subscriber<nav_msgs::msg::Odometry>> odom_subs0;
   std::shared_ptr<message_filters::Subscriber<nav_msgs::msg::Odometry>> odom_subs1;
-  std::shared_ptr<message_filters::Subscriber<nav_msgs::msg::Odometry>> odom_subs2;
 
   using approximate_policy = message_filters::sync_policies::ApproximateTime<
-    nav_msgs::msg::Odometry,
     nav_msgs::msg::Odometry,
     nav_msgs::msg::Odometry>;
   
@@ -48,12 +46,10 @@ private:
 
   rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub0;
   rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub1;
-  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub2;
 
   void ts_callback(
     const nav_msgs::msg::Odometry::ConstSharedPtr & odom_msg0,
-    const nav_msgs::msg::Odometry::ConstSharedPtr & odom_msg1,
-    const nav_msgs::msg::Odometry::ConstSharedPtr & odom_msg2);
+    const nav_msgs::msg::Odometry::ConstSharedPtr & odom_msg1);
 };
 }  // namespace synchronizer
 
--- src/topic_synchronizer.cpp
+++ src/topic_synchronizer.cpp
@@ -18,7 +18,6 @@ namespace synchronizer
 
 using std::placeholders::_1;
 using std::placeholders::_2;
-using std::placeholders::_3;
 using std::chrono::steady_clock;
 
 TopicSynchronizer::TopicSynchronizer(const rclcpp::NodeOptions & options)
@@ -26,41 +25,34 @@ TopicSynchronizer::TopicSynchronizer(const rclcpp::NodeOptions & options)
 {
   odom_source0 = declare_parameter<std::string>("topics.source.odom0", "/odometry0");
   odom_source1 = declare_parameter<std::string>("topics.source.odom1", "/odometry1");
-  odom_source2 = declare_parameter<std::string>("topics.source.odom2", "/odometry2");
 
   odom_out0 = declare_parameter<std::string>("topics.output.odom0", "/odometry0/sync");
   odom_out1 = declare_parameter<std::string>("topics.output.odom1", "/odometry1/sync");
-  odom_out2 = declare_parameter<std::string>("topics.output.odom2", "/odometry2/sync");
 
   interval_duration = declare_parameter<float>("interval_duration", 500000000.0);
   approx_policy = declare_parameter<int>("approx_policy", 10);
 
   odom_pub0 = create_publisher<nav_msgs::msg::Odometry>(odom_out0, 10);
   odom_pub1 = create_publisher<nav_msgs::msg::Odometry>(odom_out1, 10);
-  odom_pub2 = create_publisher<nav_msgs::msg::Odometry>(odom_out2, 10);
 
   odom_subs0 = std::make_shared<message_filters::Subscriber<nav_msgs::msg::Odometry>>(this, odom_source0);
   odom_subs1 = std::make_shared<message_filters::Subscriber<nav_msgs::msg::Odometry>>(this, odom_source1);
-  odom_subs2 = std::make_shared<message_filters::Subscriber<nav_msgs::msg::Odometry>>(this, odom_source2);
 
   time_sync = std::make_shared<message_filters::Synchronizer<approximate_policy>>(
     approximate_policy(approx_policy),
     *odom_subs0,
-    *odom_subs1,
-    *odom_subs2);
+    *odom_subs1);
 
   time_sync->setMaxIntervalDuration(rclcpp::Duration(0, interval_duration));
   time_sync->registerCallback(
-    std::bind(&TopicSynchronizer::ts_callback, this, _1, _2, _3));
+    std::bind(&TopicSynchronizer::ts_callback, this, _1, _2));
 }
 
 void TopicSynchronizer::ts_callback(
   const nav_msgs::msg::Odometry::ConstSharedPtr & odom_msg0,
-  const nav_msgs::msg::Odometry::ConstSharedPtr & odom_msg1,
-  const nav_msgs::msg::Odometry::ConstSharedPtr & odom_msg2)
+  const nav_msgs::msg::Odometry::ConstSharedPtr & odom_msg1)
 {
   odom_pub0->publish(*odom_msg0);
   odom_pub1->publish(*odom_msg1);
-  odom_pub2->publish(*odom_msg2);
 }
 }  // namespace synchronizer
