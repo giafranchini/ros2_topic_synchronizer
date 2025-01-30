86e48399fe45b8021e228204d617dd56b882f382
giacomo <giacomo.franchini@polito.it>
2025-01-30 15:19:13 +0100

    lidar-camera calib

--- include/topic_synchronizer/topic_synchronizer.hpp
+++ include/topic_synchronizer/topic_synchronizer.hpp
@@ -5,12 +5,9 @@ Copyright 2024 Giacomo Franchini
 #ifndef TOPIC_SYNCHRONIZER__TOPIC_SYNCHRONIZER_HPP_
 #define TOPIC_SYNCHRONIZER__TOPIC_SYNCHRONIZER_HPP_
 
-#include <geometry_msgs/msg/transform_stamped.h>
-#include <geometry_msgs/msg/twist_stamped.h>
-#include <sensor_msgs/msg/imu.hpp>
-#include <sensor_msgs/msg/joint_state.hpp>
-#include <nav_msgs/msg/odometry.hpp>
-#include <jackal_msgs/msg/feedback.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/camera_info.hpp>
 
 #include <array>
 #include <memory>
@@ -33,47 +30,31 @@ public:
   ~TopicSynchronizer() {}
 
 private:
-  std::string cmd_vel_source, feedback_source, imu_source, joint_state_source, odom_source, odom_filtered_source, odom_gt_source;
-  std::string cmd_vel_out, feedback_out, imu_out, joint_state_out, odom_out, odom_filtered_out, odom_gt_out;
+  std::string cloud_source, image_source, camera_info_source;
+  std::string cloud_out, image_out, camera_info_out;
   
   float interval_duration, approx_policy;
 
   // Define subscribers to topics that we want to synchronize
-  std::shared_ptr<message_filters::Subscriber<geometry_msgs::msg::Twist>> cmd_vel_subs;
-  std::shared_ptr<message_filters::Subscriber<jackal_msgs::msg::Feedback>> feedback_subs;
-  std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::JointState>> joint_state_subs;
-  std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::Imu>> imu_subs;
-  std::shared_ptr<message_filters::Subscriber<nav_msgs::msg::Odometry>> odom_subs;
-  std::shared_ptr<message_filters::Subscriber<nav_msgs::msg::Odometry>> odom_filtered_subs;
-  std::shared_ptr<message_filters::Subscriber<nav_msgs::msg::Odometry>> odom_gt_subs;
+  std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::PointCloud2>> cloud_subs;
+  std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::Image>> image_subs;
+  std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::CameraInfo>> camera_info_subs;
 
   using approximate_policy = message_filters::sync_policies::ApproximateTime<
-    geometry_msgs::msg::Twist,
-    jackal_msgs::msg::Feedback,
-    sensor_msgs::msg::JointState,
-    sensor_msgs::msg::Imu,
-    nav_msgs::msg::Odometry,
-    nav_msgs::msg::Odometry,
-    nav_msgs::msg::Odometry>;
+    sensor_msgs::msg::PointCloud2,
+    sensor_msgs::msg::Image,
+    sensor_msgs::msg::CameraInfo>;
   
   std::shared_ptr<message_filters::Synchronizer<approximate_policy>> time_sync;
 
-  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub;
-  rclcpp::Publisher<jackal_msgs::msg::Feedback>::SharedPtr feedback_pub;
-  rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_state_pub;
-  rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub;
-  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub;
-  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_filtered_pub;
-  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_gt_pub;
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr cloud_pub;
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_pub;
+  rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr camera_info_pub;
 
   void ts_callback(
-    const geometry_msgs::msg::Twist::ConstSharedPtr & cmd_vel_msg,
-    const jackal_msgs::msg::Feedback::ConstSharedPtr & feedback_msg,
-    const sensor_msgs::msg::JointState::ConstSharedPtr & joint_state_msg,
-    const sensor_msgs::msg::Imu::ConstSharedPtr & imu_msg,
-    const nav_msgs::msg::Odometry::ConstSharedPtr & odom_msg,
-    const nav_msgs::msg::Odometry::ConstSharedPtr & odom_filtered_msg,
-    const nav_msgs::msg::Odometry::ConstSharedPtr & odom_gt_msg);
+    const sensor_msgs::msg::PointCloud2::ConstSharedPtr & cloud_msg,
+    const sensor_msgs::msg::Image::ConstSharedPtr & image_msg,
+    const sensor_msgs::msg::CameraInfo::ConstSharedPtr & camera_info_msg);
 };
 }  // namespace synchronizer
 
--- src/topic_synchronizer.cpp
+++ src/topic_synchronizer.cpp
@@ -19,85 +19,49 @@ namespace synchronizer
 using std::placeholders::_1;
 using std::placeholders::_2;
 using std::placeholders::_3;
-using std::placeholders::_4;
-using std::placeholders::_5;
-using std::placeholders::_6;
-using std::placeholders::_7;
 using std::chrono::steady_clock;
 
 TopicSynchronizer::TopicSynchronizer(const rclcpp::NodeOptions & options)
 : Node("topic_synchronizer", options)
 {
-  cmd_vel_source = declare_parameter<std::string>("topics.source.cmd_vel", "/cmd_vel");
-  feedback_source = declare_parameter<std::string>("topics.source.feedback", "/feedback");
-  joint_state_source = declare_parameter<std::string>("topics.source.joint_state", "/joint_states");
-  imu_source = declare_parameter<std::string>("topics.source.imu", "/imu");
-  odom_source = declare_parameter<std::string>("topics.source.odom", "/odometry");
-  odom_filtered_source = declare_parameter<std::string>("topics.source.odom_filtered", "/odometry_filtered");
-  odom_gt_source = declare_parameter<std::string>("topics.source.odom_gt", "/odometry_gt");
+  cloud_source = declare_parameter<std::string>("topics.source.cloud", "/cloud");
+  image_source = declare_parameter<std::string>("topics.source.image", "/image");
+  camera_info_source = declare_parameter<std::string>("topics.source.camera_info", "/camera_info");
 
-  cmd_vel_out = declare_parameter<std::string>("topics.output.cmd_vel", "/cmd_vel_sync");
-  feedback_out = declare_parameter<std::string>("topics.output.feedback", "/feedback_sync");
-  joint_state_out = declare_parameter<std::string>("topics.output.joint_state", "/joint_states_sync");
-  imu_out = declare_parameter<std::string>("topics.output.imu", "/imu_sync");
-  odom_out = declare_parameter<std::string>("topics.output.odom", "/odometry_sync");
-  odom_filtered_out = declare_parameter<std::string>("topics.output.odom_filtered", "/odometry_filtered_sync");
-  odom_gt_out = declare_parameter<std::string>("topics.output.odom_gt", "/odometry_gt_sync");
+  cloud_out = declare_parameter<std::string>("topics.output.cloud", "/cloud_sync");
+  image_out = declare_parameter<std::string>("topics.output.image", "/image_sync");
+  camera_info_out = declare_parameter<std::string>("topics.output.camera_info", "/camera_info_sync");
 
   interval_duration = declare_parameter<float>("interval_duration", 500000000.0);
   approx_policy = declare_parameter<double>("approx_policy", 10.0);
 
-  cmd_vel_pub = create_publisher<geometry_msgs::msg::Twist>(cmd_vel_out, 10);
-  feedback_pub = create_publisher<jackal_msgs::msg::Feedback>(feedback_out, 10);
-  joint_state_pub = create_publisher<sensor_msgs::msg::JointState>(joint_state_out, 10);
-  imu_pub = create_publisher<sensor_msgs::msg::Imu>(imu_out, 10);
-  odom_pub = create_publisher<nav_msgs::msg::Odometry>(odom_out, 10);
-  odom_filtered_pub = create_publisher<nav_msgs::msg::Odometry>(odom_filtered_out, 10);
-  odom_gt_pub = create_publisher<nav_msgs::msg::Odometry>(odom_gt_out, 10);
+  cloud_pub = create_publisher<sensor_msgs::msg::PointCloud2>(cloud_out, 10);
+  image_pub = create_publisher<sensor_msgs::msg::Image>(image_out, 10);
+  camera_info_pub = create_publisher<sensor_msgs::msg::CameraInfo>(camera_info_out, 10);
 
-  cmd_vel_subs = std::make_shared<message_filters::Subscriber<geometry_msgs::msg::Twist>>(this, cmd_vel_source);
-  feedback_subs = std::make_shared<message_filters::Subscriber<jackal_msgs::msg::Feedback>>(this, feedback_source);
-  joint_state_subs = std::make_shared<message_filters::Subscriber<sensor_msgs::msg::JointState>>(this, joint_state_source);
-  imu_subs = std::make_shared<message_filters::Subscriber<sensor_msgs::msg::Imu>>(this, imu_source);
-  odom_subs = std::make_shared<message_filters::Subscriber<nav_msgs::msg::Odometry>>(this, odom_source);
-  odom_filtered_subs = std::make_shared<message_filters::Subscriber<nav_msgs::msg::Odometry>>(this, odom_filtered_source);
-  odom_gt_subs = std::make_shared<message_filters::Subscriber<nav_msgs::msg::Odometry>>(this, odom_gt_source);
+  cloud_subs = std::make_shared<message_filters::Subscriber<sensor_msgs::msg::PointCloud2>>(this, cloud_source);
+  image_subs = std::make_shared<message_filters::Subscriber<sensor_msgs::msg::Image>>(this, image_source);
+  camera_info_subs = std::make_shared<message_filters::Subscriber<sensor_msgs::msg::CameraInfo>>(this, camera_info_source);
 
   time_sync = std::make_shared<message_filters::Synchronizer<approximate_policy>>(
     approximate_policy(approx_policy),
-    *cmd_vel_subs,
-    *feedback_subs,
-    *joint_state_subs,
-    *imu_subs,
-    *odom_subs,
-    *odom_filtered_subs,
-    *odom_gt_subs);
+    *cloud_subs,
+    *image_subs,
+    *camera_info_subs);
 
   time_sync->setMaxIntervalDuration(rclcpp::Duration(0, interval_duration));
   time_sync->registerCallback(
-    std::bind(&TopicSynchronizer::ts_callback, this, _1, _2, _3, _4, _5, _6, _7));
+    std::bind(&TopicSynchronizer::ts_callback, this, _1, _2, _3));
 }
 
 void TopicSynchronizer::ts_callback(
-  const geometry_msgs::msg::Twist::ConstSharedPtr & cmd_vel_msg,
-  const jackal_msgs::msg::Feedback::ConstSharedPtr & feedback_msg,
-  const sensor_msgs::msg::JointState::ConstSharedPtr & joint_state_msg,
-  const sensor_msgs::msg::Imu::ConstSharedPtr & imu_msg,
-  const nav_msgs::msg::Odometry::ConstSharedPtr & odom_msg,
-  const nav_msgs::msg::Odometry::ConstSharedPtr & odom_filtered_msg,
-  const nav_msgs::msg::Odometry::ConstSharedPtr & odom_gt_msg)
+  const sensor_msgs::msg::PointCloud2::ConstSharedPtr & cloud_msg,
+  const sensor_msgs::msg::Image::ConstSharedPtr & image_msg,
+  const sensor_msgs::msg::CameraInfo::ConstSharedPtr & camera_info_msg)
 { 
   RCLCPP_DEBUG(get_logger(), "Received messages");
-  RCLCPP_DEBUG(get_logger(), 
-    "Publishing messages at time: \n%f \n%f \n%f", 
-    feedback_msg->header.stamp.sec + feedback_msg->header.stamp.nanosec * 1e-9);
- 
-  cmd_vel_pub->publish(*cmd_vel_msg);
-  feedback_pub->publish(*feedback_msg);
-  joint_state_pub->publish(*joint_state_msg);
-  imu_pub->publish(*imu_msg);
-  odom_pub->publish(*odom_msg);
-  odom_filtered_pub->publish(*odom_filtered_msg);
-  odom_gt_pub->publish(*odom_gt_msg);
+  cloud_pub->publish(*cloud_msg);
+  image_pub->publish(*image_msg);
+  camera_info_pub->publish(*camera_info_msg);
 }
 }  // namespace synchronizer
